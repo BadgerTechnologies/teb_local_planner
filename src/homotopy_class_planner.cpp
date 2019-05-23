2b5e7ce055484533afe2879682b28e9fd86caf32
Howard Cochran <howard@badger-technologies.com>
2019-05-22 20:59:32 -0400

    HCP: Allow a tunable number of TEBs in same class as "best"
    Prior to this change, excess loops could develop in the plan. Sometimes
    the initial teb would turn 270-degrees to the left rather than 90
    degrees to the right, especially when the goal is close to an obstacle.
    In other cases, if a dynamic obstacle moved through the scene, it could
    latch a 360-degree loop in the plan. Because such looped plans are a
    local minimum, the optimizer is unable to escape from them.
    
    A simple way to demonstrate this problem is to use a teleop joystick to
    cause the robot to turn in place while it is navigating. Each
    turn-in-place adds a loop to the plan so that the robot would "unwind"
    all of these loops as it proceeds to the goal!
    
    This change introduces a new parameter
    max_number_plans_in_current_class. When set to 2 or more, it will allow
    the HomotopyClassPlanner to accept multiple candidate plans that are in
    the same class as the current "best" plan. This gives a chance for
    exploration to add a new plan in the same homotopy class as the current
    one, so that it can discover a more optimal plan without the loops.
    
    NOTE: The actual number of homotopy classes being considered is reduced
    by (max_number_plans_in_current_class - 1), so one should generally
    increase max_number_classes whenever this parameter is increased. It
    makes no sense for this parameter to be greater than max_number_classes.

--- src/homotopy_class_planner.cpp
+++ src/homotopy_class_planner.cpp
@@ -199,7 +199,12 @@ bool HomotopyClassPlanner::addEquivalenceClassIfNew(const EquivalenceClassPtr& e
   }
 
   if (hasEquivalenceClass(eq_class))
-    return false;
+  {
+    // Allow up to configured number of Tebs that are in the same homotopy
+    // class as the current (best) Teb to avoid being stuck in a local minimum
+    if (!isInBestTebClass(eq_class) || numTebsInBestTebClass() >= cfg_->hcp.max_number_plans_in_current_class)
+      return false;
+  }
 
   // Homotopy class not found -> Add to class-list, return that the h-signature is new
   equivalence_classes_.push_back(std::make_pair(eq_class,lock));
@@ -235,6 +240,10 @@ void HomotopyClassPlanner::renewAndAnalyzeOldTebs(bool delete_detours)
                                                                       it_teb->get()->teb().timediffs().begin(), it_teb->get()->teb().timediffs().end());
 
 //     teb_candidates.push_back(std::make_pair(it_teb,H));
+    if (*it_teb == best_teb_)
+    {
+      best_teb_eq_class_ = equivalence_class;
+    }
 
     // WORKAROUND until the commented code below works
     // Here we do not compare cost values. Just first come first serve...
@@ -374,6 +383,33 @@ TebOptimalPlannerPtr HomotopyClassPlanner::addAndInitNewTeb(const PoseSE2& start
   return TebOptimalPlannerPtr();
 }
 
+
+bool HomotopyClassPlanner::isInBestTebClass(const EquivalenceClassPtr& eq_class) const
+{
+  bool answer = false;
+  if (best_teb_eq_class_)
+    answer = best_teb_eq_class_->isEqual(*eq_class);
+  return answer;
+}
+
+int HomotopyClassPlanner::numTebsInClass(const EquivalenceClassPtr& eq_class) const
+{
+  int count = 0;
+  for (const std::pair<EquivalenceClassPtr, bool>& eqrel : equivalence_classes_)
+  {
+    if (eq_class->isEqual(*eqrel.first))
+      ++count;
+  }
+  return count;
+}
+
+int HomotopyClassPlanner::numTebsInBestTebClass() const
+{
+  int count = 0;
+  if (best_teb_eq_class_)
+    count = numTebsInClass(best_teb_eq_class_);
+  return count;
+}
 
 TebOptimalPlannerPtr HomotopyClassPlanner::addAndInitNewTeb(const std::vector<geometry_msgs::PoseStamped>& initial_plan, const geometry_msgs::Twist* start_velocity)
 {
