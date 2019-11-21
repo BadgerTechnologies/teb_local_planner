BadgerTechnologies/teb_local_planner

commit 682ad2403fa43e3646aad79381ceae4e69580973

bugfix in calculateHSignature. Fixes #90.

include/teb_local_planner/h_signature.h
@@ -155,21 +155,21 @@ public:
                 cplx Al = f0;
                 for (std::size_t j=0; j<obstacles->size(); ++j)
                 {
-                if (j==l)
-                    continue;
-                cplx obst_j = obstacles->at(j)->getCentroidCplx();
-                cplx diff = obst_l - obst_j;
-                //if (diff.real()!=0 || diff.imag()!=0)
-                if (std::abs(diff)<0.05) // skip really close obstacles
-                    Al /= diff;
-                else
-                    continue;
+                    if (j==l)
+                        continue;
+                    cplx obst_j = obstacles->at(j)->getCentroidCplx();
+                    cplx diff = obst_l - obst_j;
+                    //if (diff.real()!=0 || diff.imag()!=0)
+                    if (std::abs(diff)<0.05) // skip really close obstacles
+                        continue;
+                     else
+                        Al /= diff;
                 }
                 // compute log value
                 double diff2 = std::abs(z2-obst_l);
                 double diff1 = std::abs(z1-obst_l);
                 if (diff2 == 0 || diff1 == 0)
-                continue;
+                    continue;
                 double log_real = std::log(diff2)-std::log(diff1);
                 // complex ln has more than one solution -> choose minimum abs angle -> paper
                 double arg_diff = std::arg(z2-obst_l)-std::arg(z1-obst_l);

commit 9d88787a05c7fd298c26f1004a70b9152a172a2e

h_signature: Revert to power function for f0 (numerator)
A previous bugfix for Issue #90 corrected the denominator of Al which
was generally being left equal to (1, 0) before that fix. After that
fix, the denominator generally becomes very large if there are more than
a few obstacles in the scene (e.g. With ~30 obstacles, the denominator
was around 10^17)

In what was likely an earlier attempt to fix that caused by the
incorrect denominator calculation, the original power function used in
the numerator (f0) had been replaced by a simple multiplication.
However, now that we are calculating the denominator correctly while
still using only a small numerator, all the H-signatures have become
extremely small (on the order of 10^-16 with around 30 obstacles in my
tests).  Thus, ::isEqual() method always returns true so we never
explore any alternate plans, rendering the HCP ineffective.

This change restores the original calculation of f0 thus making it large
enough that the huge denominator will not dengenerate the H-signature to
near zero. In my tests, this makes the HCP function correctly again.

Fixes: 682ad2403fa4 ("bugfix in calculateHSignature. Fixes #90")

include/teb_local_planner/h_signature.h
@@ -146,8 +146,8 @@ public:
             for (std::size_t l=0; l<obstacles->size(); ++l) // iterate all obstacles
             {
                 cplx obst_l = obstacles->at(l)->getCentroidCplx();
-                //cplx f0 = (long double) prescaler * std::pow(obst_l-map_bottom_left,a) * std::pow(obst_l-map_top_right,b);
-                cplx f0 = (long double) cfg_->hcp.h_signature_prescaler * (long double)a*(obst_l-map_bottom_left) * (long double)b*(obst_l-map_top_right);
+                cplx f0 = (cplx::value_type) cfg_->hcp.h_signature_prescaler* std::pow(obst_l-map_bottom_left,a) * std::pow(obst_l-map_top_right,b);
+                //cplx f0 = (cplx::value_type) cfg_->hcp.h_signature_prescaler * (cplx::value_type)a*(obst_l-map_bottom_left) * (cplx::value_type)b*(obst_l-map_top_right);
 
                 // denum contains product with all obstacles exepct j==l
                 cplx Al = f0;