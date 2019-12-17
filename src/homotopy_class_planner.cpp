b23a68ee84c0b17583b80046e5035094f211e3dd
C. Andy Martin <cam@badger-technologies.com>
2019-12-17 11:26:54 -1000

    now that costmap 3d queries are thread safe, do not copy them

--- src/homotopy_class_planner.cpp
+++ src/homotopy_class_planner.cpp
@@ -368,8 +368,7 @@ TebOptimalPlannerPtr HomotopyClassPlanner::addAndInitNewTeb(const PoseSE2& start
     return TebOptimalPlannerPtr();
   TebOptimalPlannerPtr candidate =  TebOptimalPlannerPtr( new TebOptimalPlanner(*cfg_, obstacles_, robot_model_, visualization_));
   if (costmap_3d_query_ != nullptr)
-    // use a copy of the query, as queries are not thread safe
-    candidate->useCostmap3DQuery(costmap_3d::Costmap3DQueryPtr(new costmap_3d::Costmap3DQuery(*costmap_3d_query_)));
+    candidate->useCostmap3DQuery(costmap_3d_query_);
 
   candidate->teb().initTrajectoryToGoal(start, goal, 0, cfg_->robot.max_vel_x, cfg_->trajectory.min_samples, cfg_->trajectory.allow_init_with_backwards_motion);
 
@@ -423,8 +422,7 @@ TebOptimalPlannerPtr HomotopyClassPlanner::addAndInitNewTeb(const std::vector<ge
     return TebOptimalPlannerPtr();
   TebOptimalPlannerPtr candidate = TebOptimalPlannerPtr( new TebOptimalPlanner(*cfg_, obstacles_, robot_model_, visualization_));
   if (costmap_3d_query_ != nullptr)
-    // use a copy of the query, as queries are not thread safe
-    candidate->useCostmap3DQuery(costmap_3d::Costmap3DQueryPtr(new costmap_3d::Costmap3DQuery(*costmap_3d_query_)));
+    candidate->useCostmap3DQuery(costmap_3d_query_);
 
   candidate->teb().initTrajectoryToGoal(initial_plan, cfg_->robot.max_vel_x,
     cfg_->trajectory.global_plan_overwrite_orientation, cfg_->trajectory.min_samples, cfg_->trajectory.allow_init_with_backwards_motion);
